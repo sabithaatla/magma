1bf0b4ac5c92d0eb2bc57923b71a0332593f672e
aniket021997 <aniket.sonawane@wavelabs.ai>
2021-12-29 11:02:24 +0000

    test(mme): unit test for ngap state serialization and deserialization
    Signed-off-by: aniket021997 <aniket.sonawane@wavelabs.ai>

--- lte/gateway/c/core/oai/tasks/ngap/ngap_state_converter.h
+++ lte/gateway/c/core/oai/tasks/ngap/ngap_state_converter.h
@@ -37,7 +37,7 @@ extern "C" {
 #endif
 
 #include "lte/gateway/c/core/oai/include/state_converter.h"
-#include "lte/protos/oai/ngap_state.pb.h"
+#include "lte/protos/oai/s1ap_state.pb.h"
 #include "lte/gateway/c/core/oai/tasks/ngap/ngap_state.h"
 using namespace std;
 using namespace magma::lte;
@@ -47,32 +47,56 @@ namespace magma5g {
 
 class NgapStateConverter : magma::lte::StateConverter {
  public:
-  static void state_to_proto(ngap_state_t* state, oai::NgapState* proto);
+  static void state_to_proto(ngap_state_t* state, oai::S1apState* proto);
 
-  static void proto_to_state(const oai::NgapState& proto, ngap_state_t* state);
+  static void proto_to_state(const oai::S1apState& proto, ngap_state_t* state);
 
   /**
    * Serializes ngap_imsi_map_t to NgapImsiMap proto
    */
   static void ngap_imsi_map_to_proto(
-      const ngap_imsi_map_t* ngap_imsi_map, oai::NgapImsiMap* ngap_imsi_proto);
+      const ngap_imsi_map_t* ngap_imsi_map,
+      magma::lte::oai::S1apImsiMap* ngap_imsi_proto);
 
   /**
    * Deserializes ngap_imsi_map_t from NgapImsiMap proto
    */
   static void proto_to_ngap_imsi_map(
-      const oai::NgapImsiMap& ngap_imsi_proto, ngap_imsi_map_t* ngap_imsi_map);
+      const magma::lte::oai::S1apImsiMap& ngap_imsi_proto,
+      ngap_imsi_map_t* ngap_imsi_map);
 
-  static void gnb_to_proto(gnb_description_t* gnb, oai::GnbDescription* proto);
+  /**
+   * Serializes m5g_supported_ta_list_t to SupportedTaList proto
+   */
+  static void supported_ta_list_to_proto(
+      const m5g_supported_ta_list_t* supported_ta_list,
+      oai::SupportedTaList* supported_ta_list_proto);
+
+  static void proto_to_supported_ta_list(
+      m5g_supported_ta_list_t* supported_ta_list_state,
+      const oai::SupportedTaList& supported_ta_list_proto);
+
+  /**
+   * Serializes m5g_supported_tai_items_t to supported_tai_item proto
+   */
+  static void supported_tai_item_to_proto(
+      const m5g_supported_tai_items_t* state_supported_tai_item,
+      oai::SupportedTaiItems* supported_tai_item_proto);
+
+  static void proto_to_supported_tai_items(
+      m5g_supported_tai_items_t* supported_tai_item_state,
+      const oai::SupportedTaiItems& supported_tai_item_proto);
+
+  static void gnb_to_proto(gnb_description_t* gnb, oai::EnbDescription* proto);
 
   static void proto_to_gnb(
-      const oai::GnbDescription& proto, gnb_description_t* gnb);
+      const oai::EnbDescription& proto, gnb_description_t* gnb);
 
   static void ue_to_proto(
-      const m5g_ue_description_t* ue, oai::Ngap_UeDescription* proto);
+      const m5g_ue_description_t* ue, oai::UeDescription* proto);
 
   static void proto_to_ue(
-      const oai::Ngap_UeDescription& proto, m5g_ue_description_t* ue);
+      const oai::UeDescription& proto, m5g_ue_description_t* ue);
 
  private:
   NgapStateConverter();
